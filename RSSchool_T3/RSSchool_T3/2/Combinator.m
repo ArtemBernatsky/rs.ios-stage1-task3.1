4a2d5ccd4ca697dd496f1ef5130d78367dc4bdd2
Artem Bernatsky <bernatskyhome@gmail.com>
2020-04-05 18:45:37 +0300

    feat: second task

--- RSSchool_T3/RSSchool_T3/2/Combinator.m
+++ RSSchool_T3/RSSchool_T3/2/Combinator.m
@@ -2,6 +2,33 @@
 
 @implementation Combinator
 - (NSNumber*)chechChooseFromArray:(NSArray <NSNumber*>*)array {
-    return @0;
+    long posters = [[array objectAtIndex: 0] integerValue];
+    long colors = [[array objectAtIndex: 1] integerValue];
+
+    for (int n = 1; n < colors; n++) {
+        long colorsFactorial = [self getFactorial: colors];
+        long nFactorial = [self getFactorial: n];
+        long colorsMinusNFactorial = [self getFactorial:(colors - n)];
+        long combinations =  colorsFactorial/ (nFactorial * colorsMinusNFactorial);
+        
+        if (posters == combinations) {
+            return [NSNumber numberWithInt: n];
+        }
+
+    }
+    return nil;
+}
+
+- (NSUInteger) getFactorial:(long)n {
+    
+    if (n < 0) {
+        return -1;
+    }
+    
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+
+    return n * [self getFactorial: n - 1];
 }
 @end
